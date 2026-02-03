33ebaf538d465e1b7dba8d3df28e5d4f7a25e7b0
aminat <aminatabidemi212@gmail.com>
2026-02-03 18:57:55 +0100

    backend deployment

--- SDBMS/wsgi.py
+++ SDBMS/wsgi.py
@@ -3,7 +3,7 @@ from django.core.wsgi import get_wsgi_application
 
 # Tell Django to use the production settings if we're on Vercel
 if os.environ.get('VERCEL'):
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SDBMS.settings_production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SDBMS.settings_prod')
 else:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SDBMS.settings')
 
